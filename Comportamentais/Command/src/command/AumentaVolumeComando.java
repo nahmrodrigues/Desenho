16a234e40d867ad47a4e6719158ead36f852fb65
Natália Rodrigues <nataliamrq@gmail.com>
2018-10-15 13:49:19 -0300

    Adding Command to behavioral patterns.

---
+++ Comportamentais/Command/src/command/AumentaVolumeComando.java
@@ -0,0 +1,16 @@
+package command;
+
+public class AumentaVolumeComando implements Comando{
+	private Player player;
+	private int levels;
+	
+	public AumentaVolumeComando(Player player, int levels) {
+		this.player = player;
+		this.levels = levels;
+	}
+	
+	public void executa() {
+		this.player.increaseVolume(this.levels);
+	}
+
+}
---
+++ Comportamentais/Command/src/command/ListaDeComandos.java
@@ -0,0 +1,19 @@
+package command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaDeComandos {
+	private List<Comando> comandos = new ArrayList<Comando>();
+	
+	public void adiciona(Comando comando) {
+		this.comandos.add(comando);
+	}
+	
+	public void executa() {
+		for(Comando comando : this.comandos) {
+			comando.executa();
+		}
+	}
+
+}
