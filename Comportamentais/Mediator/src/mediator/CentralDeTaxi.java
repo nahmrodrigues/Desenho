d4b80fac759e4d81c832f7501f0450b24a916d6a
Natália Rodrigues <nataliamrq@gmail.com>
2018-10-15 15:58:05 -0300

    Adding Mediator to behavioral patterns.

---
+++ Comportamentais/Mediator/src/mediator/CentralDeTaxi.java
@@ -0,0 +1,39 @@
+package mediator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CentralDeTaxi {
+	private List<Taxi> taxisLivres = new ArrayList<Taxi>();
+	private List<Passageiro> passageirosEmEspera = new ArrayList<Passageiro>();
+	
+	public void adicionaTaxiDisponivel(Taxi taxi) {
+		System.out.println("Táxi " + taxi.getId() + " voltou para a fila");
+		taxisLivres.add(taxi);
+	}
+	
+	public void pedeTaxi(Passageiro passageiro) {
+		Taxi taxi = this.esperaTaxi(passageiro);
+		System.out.println("Táxi " + taxi.getId() + " levando " + passageiro.getNome());
+		taxi.atende();
+	}
+	
+	private Taxi esperaTaxi(Passageiro passageiro) {
+		
+		this.passageirosEmEspera.add(passageiro);
+		
+		synchronized(this) {
+			while(this.taxisLivres.isEmpty() || this.passageirosEmEspera.get(0).equals(passageiro)) {
+				try {
+					this.wait();
+				}
+				catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			this.passageirosEmEspera.remove(0);
+			return this.taxisLivres.remove(0);
+		}
+	}
+}
